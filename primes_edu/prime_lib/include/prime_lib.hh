12db30fde3ac5650b346d19f3a7d63fc37bed3d2
Ilyas <ilyas.oulkadda@epita.fr>
2023-06-12 21:44:41 +0200

    added primes implem

--- primes_edu/prime_lib/include/prime_lib.hh
+++ primes_edu/prime_lib/include/prime_lib.hh
@@ -1,5 +1,8 @@
 #pragma once
-
+#include <vector>
+#include <cmath>
+#include <ranges>
+#include <algorithm>
 // The iterator class goes either here
 // class /*good name*/ {
 // };
@@ -7,10 +10,24 @@
 class prime_number_range {
 private:
   std::size_t count_; // Number of prime numbers to generate
+  std::vector<int> prime_numbers;
 
   //TODO
 
 public:
+  class iterator {
+    public:
+      iterator(prime_number_range& range, int curr);
+      prime_number_range& range;
+      int curr;
+      int operator*() const;
+      iterator operator++();
+      bool operator!=(const iterator& other) const;
+  };
+
+  iterator begin();
+  iterator end();
+  prime_number_range(std::size_t count);
   // Or here
   // class /*good name*/ {
   // };
---
+++ primes_edu/prime_lib/prime.cc
@@ -0,0 +1,47 @@
+#include "include/prime_lib.hh"
+
+
+prime_number_range::iterator::iterator(prime_number_range& range, int curr)
+    : range(range), curr(curr)
+    {}
+
+int prime_number_range::iterator::operator*() const{
+    return range.prime_numbers[curr];
+}
+
+prime_number_range::iterator prime_number_range::iterator::operator++(){
+    curr++;
+    return *this;
+}
+
+bool prime_number_range::iterator::operator!=(const prime_number_range::iterator& other) const{
+    return curr != other.curr;
+}
+prime_number_range::iterator prime_number_range::begin(){
+    return iterator(*this, 0);
+}
+
+prime_number_range::iterator prime_number_range::end(){
+    return iterator(*this, prime_numbers.size());
+}
+
+prime_number_range::prime_number_range(std::size_t count)
+    : count_(count)
+    {
+        std::size_t X = 0, i = 2;
+        bool flag;
+        while(X < count){
+            flag = true;
+            for(std::size_t j = 2; j <= std::sqrt(i); j++){
+                if (i%j == 0){
+                    flag = false;
+                    break;
+                }
+            }
+            if(flag){
+                prime_numbers.push_back(i);
+                X++;
+            }
+            i++;
+        }
+    }
